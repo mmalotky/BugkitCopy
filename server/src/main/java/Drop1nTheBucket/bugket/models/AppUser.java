b899200a28270762ac66a217692fcd106c048d62
Martin Malotky <mmalotky@dev-10.com>
2023-01-03 15:57:55 -0600

    + server initial setup + server dependencies + sql user

---
+++ server/src/main/java/Drop1nTheBucket/bugket/models/AppUser.java
@@ -0,0 +1,67 @@
+package Drop1nTheBucket.bugket.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AppUser implements UserDetails {
+    private int id;
+    private String username;
+    private String password;
+    private Collection<GrantedAuthority> authorities;
+    private boolean enabled;
+
+    public AppUser(int id, String username, String password, boolean enabled, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+        this.authorities = convertRolesToAuthorities(roles);
+    }
+
+    private Collection<GrantedAuthority> convertRolesToAuthorities(List<String> roles) {
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>(authorities);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
