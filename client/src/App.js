8e99f5fff2fefc90c922c7415208dc5c8c5db23c
Martin Malotky <mmalotky@dev-10.com>
2023-01-03 14:58:20 -0600

    added bootstrap and basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <p>Hello, World!</p>
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path='contact' element={<Contact/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/components/NotFound.js
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+        <div className="container">
+            <h3>404</h3>
+            <p>Page not found</p>
+        </div>
+    );
+}
+  
+export default NotFound;
