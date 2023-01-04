5e598ddb31b4d7650aec9f1b93cdbaaeca17ae0d
Alex Campbell <alexcampbell1124@gmail.com>
2023-01-04 11:38:45 -0500

    updated services

---
+++ server/src/test/java/Drop1nTheBucket/bugket/domain/AppUserServiceTest.java
@@ -0,0 +1,25 @@
+package Drop1nTheBucket.bugket.domain;
+
+import Drop1nTheBucket.bugket.data.AppUserRepository;
+import Drop1nTheBucket.bugket.models.AppUser;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class AppUserServiceTest {
+
+    @Autowired
+    AppUserService service;
+
+    @MockBean
+    AppUserRepository repository;
+
+    @Test
+    void findByUsername() {
+        AppUser expected = service.findByUsername("test");
+        when(repository.findByUsername("test")).thenReturn(expected);
+    }
+}
