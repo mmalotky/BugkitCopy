056b3fbd319101e45b4394b7c47efce7c11cd0d4
Keegan Kirkwood <kkirkwood@dev-10.com>
2023-01-04 12:30:20 -0500

    security

---
+++ server/src/main/java/Drop1nTheBucket/bugket/controllers/AuthController.java
@@ -0,0 +1,51 @@
+package Drop1nTheBucket.bugket.controllers;
+
+import Drop1nTheBucket.bugket.security.JwtConverter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtConverter jwtConverter;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtConverter jwtConverter) {
+        this.authenticationManager = authenticationManager;
+        this.jwtConverter = jwtConverter;
+    }
+
+    @PostMapping("/api/authenticate")
+    public ResponseEntity<?> authenticate(@RequestBody Map<String, String> credentials) {
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(credentials.get("username"), credentials.get("password"));
+        try {
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+            if (authentication.isAuthenticated()) {
+                String jwtToken = jwtConverter.getTokenFromUser((UserDetails) authentication.getPrincipal());
+
+                HashMap<String, String> map = new HashMap<>();
+                map.put("jwt_token", jwtToken);
+
+                return new ResponseEntity<>(map, HttpStatus.OK);
+            }
+        } catch (AuthenticationException ex) {
+            System.out.println(ex);
+        }
+
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+}
--- server/src/main/java/Drop1nTheBucket/bugket/domain/AppUserService.java
+++ server/src/main/java/Drop1nTheBucket/bugket/domain/AppUserService.java
@@ -2,10 +2,14 @@ package Drop1nTheBucket.bugket.domain;
 
 import Drop1nTheBucket.bugket.data.AppUserRepository;
 import Drop1nTheBucket.bugket.models.AppUser;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AppUserService {
+public class AppUserService implements UserDetailsService {
+
 
     private final AppUserRepository appUserRepository;
 
@@ -13,7 +17,8 @@ public class AppUserService {
         this.appUserRepository = appUserRepository;
     }
 
-    public AppUser findByUsername(String username){
+    @Override
+    public AppUser loadUserByUsername(String username) throws UsernameNotFoundException{
         return appUserRepository.findByUsername(username);
     }
 
