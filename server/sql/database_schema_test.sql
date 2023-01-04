778f8fb123abe2371d837eac86d829a23a6767b4
Alex Campbell <alexcampbell1124@gmail.com>
2023-01-04 17:48:16 -0500

    added edit role functionality

--- server/sql/database_schema_test.sql
+++ server/sql/database_schema_test.sql
@@ -32,14 +32,38 @@ create table reports (
 		references registered_user(user_id)
 );
 
+create table votes (
+	user_id int not null,
+    report_id int not null,
+    constraint fk_votes_registered_user
+		foreign key (user_id)
+        references registered_user(user_id),
+	constraint fk_votes_reports
+		foreign key (report_id)
+        references reports(report_id)
+);
+
 delimiter //
 create procedure set_known_good_state()
 begin
-	delete from registered_user;
-    alter table registered_user auto_increment =1;
+	delete from votes;
+    delete from reports;
+    alter table reports auto_increment = 1;
+
+    delete from registered_user;
+    alter table registered_user auto_increment = 1;
 
     insert into registered_user(username,password_hash, role_id) values
-	("admin", "$2a$12$M97L0g/BETfVkdrWu98lWu29w1T232KW8CtJ8Q4XfP/NISiEy71xq", 3);
+		("admin", "$2a$12$M97L0g/BETfVkdrWu98lWu29w1T232KW8CtJ8Q4XfP/NISiEy71xq", 3),
+        ('test', "$2a$10$7JquBL6mi2OO85djCq4jUecR/aKurpmW8Niv1ohxNtoJdoNZPrcKK", 1);
+
+    insert into reports(title, issue_description, replication_instructions, date_of_reporting, completion_status, user_id) values
+		("Bad Error", "It broke my computer", "Throw the computer into the lake", "2022-10-12", 0, 2),
+        ("Won't turn on", "My laptop won't turn on", "Watch all of Game of Thrones while unplugged", "2022-02-06", 0, 1);
+
+	insert into votes (user_id, report_id) values
+		(1, 1),
+        (2, 2);
 end //
 delimiter ;
 
@@ -49,4 +73,5 @@ insert into role (`name`) values
     ('ADMIN');
 
 insert into registered_user(username,password_hash, role_id) values
-("admin", "$2a$12$M97L0g/BETfVkdrWu98lWu29w1T232KW8CtJ8Q4XfP/NISiEy71xq", 3);
+    ("admin", "$2a$12$M97L0g/BETfVkdrWu98lWu29w1T232KW8CtJ8Q4XfP/NISiEy71xq", 3),
+    ('test', "$2a$10$7JquBL6mi2OO85djCq4jUecR/aKurpmW8Niv1ohxNtoJdoNZPrcKK", 1);
