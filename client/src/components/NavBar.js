3ad1b6f59a3df30489fa0410e9e2d544b1276b5c
Keegan Kirkwood <kkirkwood@dev-10.com>
2023-01-09 16:01:42 -0500

    Added Report Form

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,49 +2,74 @@ import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 
-function NavBar({setUser}) {
-    const navigate = useNavigate();
-    const context = useContext(AuthContext);
-    let admin = false;
-    if(context) {
-        admin = context.userData.authorities.includes("ADMIN");
-    }
+function NavBar({ setUser }) {
+  const navigate = useNavigate();
+  const context = useContext(AuthContext);
+  let admin = false;
+  if (context) {
+    admin = context.userData.authorities.includes("ADMIN");
+  }
 
-    const handleLogout = function () {
-        localStorage.setItem("userData", null);
-        setUser(null);
-        navigate("/");
-    }
+  const handleLogout = function () {
+    localStorage.setItem("userData", null);
+    setUser(null);
+    navigate("/");
+  };
 
-    return (
-        <nav className="navbar bg-dark sticky-top">
-            <div className="container-fluid">
-                <ul className="nav navbar">
-                    <li><Link className="nav-link text-white" to="/">Home</Link></li>
-                    <li><Link className="nav-link text-white" to="/bugs">View Bugs</Link></li>
-                    <li><Link className="nav-link text-white" to="/contact">Contact Us</Link></li>
-                    {
-                        admin ?
-                        <Link className="nav-link text-white" to="/edit_permissions">Edit Permissions</Link> :
-                        <></>
-                    }
-                </ul>
-                <ul className="nav navbar navbar-right">
-                    {
-                        context ?
-                        <></> :
-                        <Link className="btn btn-primary mr-4" to="/create_account">Create Account</Link>
-                    }
+  return (
+    <nav className="navbar bg-dark sticky-top">
+      <div className="container-fluid">
+        <ul className="nav navbar">
+          <li>
+            <Link className="nav-link text-white" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link text-white" to="/bugs">
+              View Bugs
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link text-white" to="/add">
+              Add a Bug
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link text-white" to="/contact">
+              Contact Us
+            </Link>
+          </li>
+          {admin ? (
+            <Link className="nav-link text-white" to="/edit_permissions">
+              Edit Permissions
+            </Link>
+          ) : (
+            <></>
+          )}
+        </ul>
+        <ul className="nav navbar navbar-right">
+          {context ? (
+            <></>
+          ) : (
+            <Link className="btn btn-primary mr-4" to="/create_account">
+              Create Account
+            </Link>
+          )}
 
-                    {
-                        context ?
-                        <button onClick={handleLogout} className="btn btn-danger">Logout</button> :
-                        <Link to="/login" className="btn btn-primary">Login</Link>
-                    }
-                </ul>
-            </div>
-        </nav>
-    );
+          {context ? (
+            <button onClick={handleLogout} className="btn btn-danger">
+              Logout
+            </button>
+          ) : (
+            <Link to="/login" className="btn btn-primary">
+              Login
+            </Link>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
