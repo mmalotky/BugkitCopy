d96c01c7b2336b482ec3d4ec42dc22fa9a1f423f
Martin Malotky <mmalotky@dev-10.com>
2023-01-11 10:58:17 -0600

    added MessageController

---
+++ server/src/main/java/Drop1nTheBucket/bugket/domain/MessageService.java
@@ -0,0 +1,19 @@
+package Drop1nTheBucket.bugket.domain;
+
+import Drop1nTheBucket.bugket.models.Message;
+
+import java.util.List;
+
+public class MessageService {
+    public List<Message> getMessagesById(int reportId) {
+        return null;
+    }
+
+    public Result<Message> add(Message message) {
+        return null;
+    }
+
+    public Result<Void> delete(int messageId) {
+        return null;
+    }
+}
