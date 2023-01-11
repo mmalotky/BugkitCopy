a37eff7795e8a581c5ae39c52f3c9138a94ce0d3
Martin Malotky <mmalotky@dev-10.com>
2023-01-11 12:07:10 -0600

    +last

--- server/src/main/java/Drop1nTheBucket/bugket/domain/MessageService.java
+++ server/src/main/java/Drop1nTheBucket/bugket/domain/MessageService.java
@@ -1,9 +1,11 @@
 package Drop1nTheBucket.bugket.domain;
 
 import Drop1nTheBucket.bugket.models.Message;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MessageService {
     public List<Message> getMessagesById(int reportId) {
         return null;
