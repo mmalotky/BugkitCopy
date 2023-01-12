05730e3f214a48a6282ea91401e9e1c28e3cb809
Alex Campbell <alexcampbell1124@gmail.com>
2023-01-12 00:19:05 -0500

    more bootstrap restructuring part 3

--- client/src/components/ReportDetails.js
+++ client/src/components/ReportDetails.js
@@ -3,196 +3,179 @@ import AuthContext from "../context/AuthContext";
 import Message from "./Message";
 import MessageForm from "./MessageForm";
 
-function ReportDetails({ report, refresh, SERVER_URL }) {
-  const VOTE_URL = SERVER_URL + "/api/vote";
+function ReportDetails({report, refresh, SERVER_URL}) {
+    const VOTE_URL = SERVER_URL + "/api/vote";
+    
+    const [voted, setVoted] = useState(false);
+    const [messages, setMessages] = useState([]);
+    
+    const context = useContext(AuthContext);
 
-  const [voted, setVoted] = useState(false);
-  const [messages, setMessages] = useState([]);
-
-  const context = useContext(AuthContext);
-
-  let auth = false;
-  if (context) {
-    auth =
-      context.userData.authorities.includes("ADMIN") ||
-      context.userData.authorities.includes("DEV");
-  }
-
-  const checkVoters = function () {
-    if (!context || !report) {
-      return;
+    let auth = false;
+    if(context) {
+        auth = context.userData.authorities.includes("ADMIN") 
+        || context.userData.authorities.includes("DEV");
     }
 
-    fetch(VOTE_URL + "/check/" + report.reportId, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${context.token}`,
-      },
-    })
-      .then((response) => {
-        if (response.status == 200) {
-          console.log(response);
-          return response.json();
-        } else {
-          console.log(response);
+    const checkVoters = function () {
+        if(!context || !report) {
+            return;
         }
-      })
-      .then(setVoted);
-  };
 
-  useEffect(checkVoters, [report]);
+        fetch(VOTE_URL + "/check/" + report.reportId, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Authorization": `Bearer ${context.token}`
+            }
+        })
+        .then((response) => {
+            if(response.status == 200) {
+                console.log(response);
+                return response.json();
+            }
+            else{
+                console.log(response);
+            }
+        })
+        .then(setVoted)
+    }
+    
+    useEffect(checkVoters, [report]);
 
-  const submitVote = function () {
-    fetch(VOTE_URL + "/" + report.reportId, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${context.token}`,
-      },
-    }).then((response) => {
-      console.log(response);
-      refresh();
-    });
-  };
-
-  const removeVote = function () {
-    fetch(VOTE_URL + "/" + report.reportId, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${context.token}`,
-      },
-    }).then((response) => {
-      console.log(response);
-      refresh();
-    });
-  };
-
-  const updateStatus = function (status) {
-    fetch(
-      SERVER_URL + "/api/reports/update/" + report.reportId + "/" + status,
-      {
-        method: "PUT",
-        headers: {
-          Authorization: `Bearer ${context.token}`,
-        },
-      }
-    ).then((response) => {
-      console.log(response);
-      refresh();
-    });
-  };
-
-  const getMessages = function () {
-    if (!report) {
-      return;
+    const submitVote = function () {
+        fetch(VOTE_URL + "/" + report.reportId, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${context.token}`
+            }
+        })
+        .then((response) => {
+            console.log(response);
+            refresh();
+        })
     }
 
-    fetch(SERVER_URL + "/api/messages/" + report.reportId)
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((json) => {
-        setMessages(json);
-      });
-  };
+    const removeVote = function () {
+        fetch(VOTE_URL + "/" + report.reportId, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${context.token}`
+            }
+        })
+        .then((response) => {
+            console.log(response);
+            refresh();
+        })
+    }
 
-  useEffect(getMessages, [report]);
+    const updateStatus = function(status) {
+        fetch(SERVER_URL + "/api/reports/update/" + report.reportId + "/" + status, {
+            method: "PUT",
+            headers: {
+                "Authorization": `Bearer ${context.token}`
+            }
+        })
+        .then((response) => {
+            console.log(response);
+            refresh();
+        })
+    }
 
-  if (!report) {
-    return (
-      <div className="col m-3 p-3">
-        <h3>Report Details</h3>
-        <p>Select a report for more details.</p>
-      </div>
-    );
-  }
+    const getMessages = function () {
+        if (!report) {
+            return;
+        }
 
-  return (
-    <div className="col text-center m-3 p-3">
-      <h3>Report Details</h3>
-      <div className="p-3 text-left bg-white border">
-        <h5>{report.title}</h5>
-        {report.completionStatus ? (
-          <p className="text-success">Complete</p>
-        ) : (
-          <p className="text-danger">Incomplete</p>
-        )}
-        <p>
-          By:{" "}
-          <span className="text-info text-uppercase">
-            {report.authorUsername}
-          </span>{" "}
-          | Posted: {report.postDate}
-        </p>
+        fetch(SERVER_URL + "/api/messages/" + report.reportId)
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        })
+        .then((json) => {
+            setMessages(json);
+        })
+    }
 
-        <h6>Issue Description</h6>
-        <p>{report.issueDescription}</p>
+    useEffect(getMessages, [report]);
 
-        <h6>Replication Instructions</h6>
-        <p>{report.replicationInstructions}</p>
-      </div>
-
-      <p>Votes: {report.voteCount}</p>
-
-      {context ? (
-        voted ? (
-          <button
-            id="removeVote"
-            className="btn btn-primary m-2"
-            type="button"
-            onClick={removeVote}
-          >
-            Remove Vote
-          </button>
-        ) : (
-          <button
-            id="vote"
-            className="btn btn-primary m-2"
-            type="button"
-            onClick={submitVote}
-          >
-            Vote
-          </button>
-        )
-      ) : (
-        <></>
-      )}
-      <br />
-      {auth ? (
-        report.completionStatus ? (
-          <button
-            className="btn btn-danger m-2"
-            type="button"
-            onClick={() => updateStatus(false)}
-          >
-            Mark as Incomplete
-          </button>
-        ) : (
-          <button
-            className="btn btn-success m-2"
-            type="button"
-            onClick={() => updateStatus(true)}
-          >
-            Mark as Complete
-          </button>
-        )
-      ) : (
-        <></>
-      )}
-      <h4>Messages</h4>
-      {messages.map((message) => {
+    if(!report) {
         return (
-          <Message
-            key={message.messageId}
-            message={message}
-            SERVER_URL={SERVER_URL}
-            getMessages={getMessages}
-          />
-        );
-      })}
-    </div>
-  );
+            <div className="col m-3 p-3">
+                <h3>Report Details</h3>
+                <p>Select a report for more retails.</p>
+            </div>
+        )
+    }
+
+    return (
+        <div className="col text-center m-3 p-3">
+            <h3>Report Details</h3>
+            <div className="p-3 text-left bg-white border">
+                <h5>{report.title}</h5>
+                {
+                    report.completionStatus ?
+                    <p className="text-success">Complete</p>
+                    : <p className="text-danger">Incomplete</p>
+                }
+                <p>By: <span className="text-info text-uppercase">{report.authorUsername}</span> | Posted: {new Date(report.postDate).toLocaleDateString("en-US")}</p>
+
+                <h6>Issue Description</h6>
+                <p>{report.issueDescription}</p>
+
+                <h6>Replication Instructions</h6>
+                <p>{report.replicationInstructions}</p>
+
+                <h4 className="text-center mt-5">Messages</h4>
+            {
+                messages.length > 0 ?
+                messages.map((message) => {
+                    return <Message 
+                        key={message.messageId} 
+                        message={message}
+                        SERVER_URL={SERVER_URL}
+                        getMessages={getMessages}
+                    />
+                })
+                : <p>No current messages.</p>
+            }
+            {
+                context ?
+                <MessageForm 
+                    report={report} 
+                    SERVER_URL={SERVER_URL}
+                    getMessages={getMessages}
+                />
+                : <></>
+            }
+            </div>
+            
+
+            <p>Votes: {report.voteCount}</p>
+
+            {
+                context ? 
+                (
+                    voted ?
+                    <button className="btn btn-primary m-2" type="button" onClick={removeVote}>Remove Vote</button>
+                    : <button className="btn btn-primary m-2" type="button" onClick={submitVote}>Vote</button>
+                )
+                
+                : <></>
+            }
+            <br/>
+            {
+                auth ?
+                (
+                    report.completionStatus ?
+                    <button className="btn btn-danger m-2" type="button" onClick={() => updateStatus(false)}>Mark as Incomplete</button>
+                    : <button className="btn btn-success m-2" type="button" onClick={() => updateStatus(true)}>Mark as Complete</button>
+                )
+                : <></>
+            }
+            
+        </div>
+    );
 }
 
 export default ReportDetails;
--- client/src/components/ViewBugs.js
+++ client/src/components/ViewBugs.js
@@ -171,28 +171,8 @@ function ViewBugs({SERVER_URL}) {
     }
 
     return (
-        <div className="container d-flex wv-100 bg-light pr-0 mr-3">
-            <div className=" bg-light d-flex w-50 text-center">
-                <ReportDetails
-                    report = {report}
-                    refresh = {refresh}
-                    SERVER_URL = {SERVER_URL}
-                />
-            </div>
-            <div className="col text-center m-3 p-3 w-50">
-                    <h3>Reports List</h3>
-                    <SearchBar search={search}/>
-                    {reports.length === 0 ? <div>{ loaded ? "No Reports Found" : "Loading..."}</div> :
-                    reports.map((r) => {
-                        return <div key = {r.reportId} className={hidden.includes(r) ? "d-none" : ""}>
-                            <ReportListItem
-                                report = {r}
-                                setReport = {setReport}
-                            />
-                        </div>
-                    })}
-            </div>
-            <div className="ml-auto mr-0 bg-white w-25">
+        <div className="container-fluid row">
+            <div className="col">
                 {
                     context ?
                     <ViewFilter
@@ -210,7 +190,29 @@ function ViewBugs({SERVER_URL}) {
                     : <></>
                 }
             </div>
-            
+            <div className="col-6">
+                <div className=" bg-light text-center">
+                    <ReportDetails
+                        report = {report}
+                        refresh = {refresh}
+                        SERVER_URL = {SERVER_URL}
+                    />
+                </div>
+            </div>
+            <div className="col text-center p-3 col">
+                <h3>Reports List</h3>
+                <SearchBar search={search}/>
+                {reports.length === 0 ? <div>{ loaded ? "No Reports Found" : "Loading..."}
+                </div> :
+                reports.map((r) => {
+                    return <div key = {r.reportId} className={hidden.includes(r) ? "d-none" : ""}>
+                        <ReportListItem
+                            report = {r}
+                            setReport = {setReport}
+                            />
+                </div>
+                })}
+            </div>
         </div>
     );
 }
